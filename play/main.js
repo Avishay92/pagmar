448a5256746693ab94c184a67a9c2c56d9e6d588
ronish31 <ronish31@gmail.com>
2019-05-26 17:22:36 +0300

    Play page init with sound

--- play/main.js
+++ play/main.js
@@ -6,3 +6,58 @@ $(".back").click(function() {
     localStorage.setItem("data", JSON.stringify(data));
     location.assign("../menu");
   });
+
+  let instrument,
+  autoWahEffect,
+  phaserEffect,
+  vibratoEffect,
+  reverbEffect,
+  pitchEffectEffect,
+  distortionEffect,
+  feedbackEffect,
+  tremoloEffect;
+  instrument = defaultSoundEffects[Object.keys(defaultSoundEffects)[0]];
+
+  initializeEffects();
+  initializeInstrument();
+
+  let input;
+  let sequence = [];
+  var seq = new Tone.Sequence(function(time, note){
+    instrument.triggerAttackRelease(note, "1n");
+    console.log(note);
+  }, sequence);
+ 
+  $('#play').click(function(){
+    seq.stop();
+    let note;
+    input = document.querySelector("#input-text").value;
+    for (let i =0; i<input.length; i++){
+      let char = input[i];
+      note = data[char].note;
+      sequence.push(note);
+
+      // updateEffects(data[char].soundEffects);
+      }
+      seq = new Tone.Sequence(function(time, note){
+        instrument.triggerAttackRelease(note, "16n");
+        console.log(note);
+      }, sequence);
+      seq.start();
+      Tone.Transport.start();
+      emptySequence(input)
+  })
+
+
+  $('#stop').click(function(){
+    instrument.volume = "-10db";
+    seq.stop();
+   })
+
+   function emptySequence(input){
+    for (let i =0; i<input.length; i++){
+      let char = input[i];
+      note = data[char].note;
+      sequence.pop();
+     }
+   }
