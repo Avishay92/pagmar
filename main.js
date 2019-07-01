33c256d9fa643f57e43285d9af582822af501e93
ronish31 <ronish31@gmail.com>
2019-07-01 16:07:30 +0300

    fontune logo

--- main.js
+++ main.js
@@ -1,33 +1,45 @@
-const text = new Blotter.Text('Fontune', {
-    family: "'ploni_dl_1.1_aaabold', serif",
-    size: 180,
-    fill: '#ffffff',
-  });
+WebFont.load({
+  google: {
+    families: ["Frank Ruhl Libre:700"]
+    
+  },
+  custom: {
+      families: ['ploni_dl_1.1_aaabold'],
+      urls: ['../../fonts/fonts.css']
+  },
+  active: function (){
+    const text = new Blotter.Text('Fontune', {
+      family: "'ploni_dl_1.1_aaabold'",
+      size: 180,
+      fill: '#ffffff',
+    });
 
-  let material = new Blotter.LiquidDistortMaterial();
+    let material = new Blotter.LiquidDistortMaterial();
 
-  material.uniforms.uSpeed.value = 0.05;
-  material.uniforms.uVolatility.value = 0.1;
-  material.uniforms.uSeed.value = 0.1;
+    material.uniforms.uSpeed.value = 0;
+    material.uniforms.uVolatility.value = 0;
+    material.uniforms.uSeed.value = 0;
 
-  let blotter = new Blotter(material, {
-    texts: text
-  });
+    let blotter = new Blotter(material, {
+      texts: text
+    });
 
-  let scope = blotter.forText(text);
-  let elem = document.getElementById('plain-text');
-  scope.appendTo(elem);
+    let scope = blotter.forText(text);
+    let elem = document.getElementById('plain-text');
+    scope.appendTo(elem);
 
-  $("#plain-text").mousemove(function(e) {	
-     material.uniforms.uSpeed.value = 0.1;
-    const formula = ((e.pageX * e.pageY) / 400000) / 1.5;
-    material.uniforms.uVolatility.value = formula;
-    material.uniforms.uSeed.value = formula;
-  });
+    $("canvas").mousemove(function(e) {	
+      material.uniforms.uSpeed.value = 0.1;
+      const formula = ((e.pageX * e.pageY) / 400000) / 1.5;
+      material.uniforms.uVolatility.value = formula;
+      material.uniforms.uSeed.value = formula;
+    });
+    $("canvas").mouseleave(function(e) {	
+      material.uniforms.uSpeed.value = 0;
+      material.uniforms.uVolatility.value = 0;
+      material.uniforms.uSeed.value = 0;
   
-  $("#plain-text").mouseleave(function(e) {	
-  material.uniforms.uSpeed.value = 0.05;
-  const formula = ((e.pageX * e.pageY) / 400000) / 1.5;
-  material.uniforms.uVolatility.value = formula;
-  material.uniforms.uSeed.value = formula;
-  });
+    });
+  }
+});
+
